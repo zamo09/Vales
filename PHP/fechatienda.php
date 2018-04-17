17db7f53fc655247e6606dd96345ba7f4b6d174d
Zamo <samyy_1614@hotmail.com>
2018-04-17 17:25:34 -0500

    Se agrega visualmente la busqueda por ID

--- PHP/fechatienda.php
+++ PHP/fechatienda.php
@@ -14,5 +14,25 @@
 
 		<input type="submit" name="" value="Enviar" id="boton1">
 	</form>
+
+<!-- busqueda por ID -->
+	<form action="informedevalesId.php" method="GET"><br>
+		<label>Folio:</label>
+		<input type="text" name="numpiso" onkeypress="return valida(event)">
+		<input type="submit" name="" value="Enviar" id="boton1">
+	</form>
+	<script>
+	function valida(e){
+	    tecla = (document.all) ? e.keyCode : e.which;
+	    //Tecla de retroceso para borrar, siempre la permite
+	    if (tecla==8){
+	        return true;
+	    }  
+	    // Patron de entrada, en este caso solo acepta numeros
+	    patron =/[0-9]/;
+	    tecla_final = String.fromCharCode(tecla);
+	    return patron.test(tecla_final);
+	}
+	</script>
 </body>
 </html>
